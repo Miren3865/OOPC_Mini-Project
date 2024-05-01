b481d1a09643fe04cc11eea7f1627d2d494d3427
Miren Savani <148608354+Miren3865@users.noreply.github.com>
2024-05-01 18:18:47 +0530

    Update SRMS.cpp

--- SRMS.cpp
+++ SRMS.cpp
@@ -162,12 +162,13 @@ int main() //main function, execution of program starts from here
 int value;
 while(true) //always in Loop
 {
-cout<<"\nPress 1 to Enter data"<<endl<<endl;
-cout<<"Press 2 to Show data"<<endl<<endl;
-cout<<"Press 3 to Search data"<<endl<<endl;
-cout<<"Press 4 to Update data"<<endl<<endl;
-cout<<"Press 5 to Delete data"<<endl<<endl;
-cout<<"Press 6 to Quit"<<endl<<endl;
+cout<<endl;
+cout<<"(1)Enter data"<<endl<<endl;
+cout<<"(2)Show data"<<endl<<endl;
+cout<<"(3)Search data"<<endl<<endl;
+cout<<"(4)Update data"<<endl<<endl;
+cout<<"(5)Delete data"<<endl<<endl;
+cout<<"(6)Quit"<<endl<<endl;
 cin>>value;
 switch(value)
 {

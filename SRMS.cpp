f2ad26749064c6b364ee87ddad4928220018e375
SwayamShah31 <152516186+SwayamShah31@users.noreply.github.com>
2024-05-01 18:54:58 +0530

    Update SRMS.cpp

--- SRMS.cpp
+++ SRMS.cpp
@@ -61,12 +61,12 @@ else
 for(int i=0;i<total;i++)
 {
 cout<<"\nData of Student "<<i+1<<endl<<endl;
-cout<<"Roll No.: "<<roll_no[i]<<endl;
-cout<<"Name: "<<name[i]<<endl;
-cout<<"Class: "<<Class[i]<<endl;
-cout<<"Course: "<<course[i]<<endl;
-cout<<"Mobile No.: "<<mobile_no[i]<<endl;
-cout<<"Admission Year: "<<admission_year[i]<<endl;
+cout<<"Roll No.:: "<<roll_no[i]<<endl;
+cout<<"Name:: "<<name[i]<<endl;
+cout<<"Class:: "<<Class[i]<<endl;
+cout<<"Course:: "<<course[i]<<endl;
+cout<<"Mobile No.:: "<<mobile_no[i]<<endl;
+cout<<"Admission Year:: "<<admission_year[i]<<endl;
 }
 }
 }
@@ -85,12 +85,12 @@ for(int i=0;i<total;i++)
 {
 if(rollno==roll_no[i])
 {
-cout<<"Roll NO "<<roll_no[i]<<endl;
-cout<<"Name "<<name[i]<<endl;
-cout<<"Class "<<Class[i]<<endl;
-cout<<"Course "<<course[i]<<endl;
-cout<<"Mobile NO "<<mobile_no[i]<<endl;
-cout<<"Admission Year "<<admission_year[i]<<endl;
+cout<<"Roll No.:: "<<roll_no[i]<<endl;
+cout<<"Name:: "<<name[i]<<endl;
+cout<<"Class:: "<<Class[i]<<endl;
+cout<<"Course:: "<<course[i]<<endl;
+cout<<"Mobile No.:: "<<mobile_no[i]<<endl;
+cout<<"Admission Year:: "<<admission_year[i]<<endl;
 }
 }
 }
@@ -111,24 +111,24 @@ if(rollno==roll_no[i])
 {
 cout<<"\nPrevious data"<<endl<<endl;
 cout<<"Data of Student "<<i+1<<endl;
-cout<<"Roll NO: "<<roll_no[i]<<endl;
-cout<<"Name "<<name[i]<<endl;
-cout<<"Class "<<Class[i]<<endl;
-cout<<"Course "<<course[i]<<endl;
-cout<<"Mobile NO "<<mobile_no[i]<<endl;
-cout<<"Admission Year "<<admission_year[i]<<endl;
-cout<<"\nEnter new data"<<endl<<endl;
-cout<<"Enter Roll NO ";
+cout<<"Roll No.:: "<<roll_no[i]<<endl;
+cout<<"Name:: "<<name[i]<<endl;
+cout<<"Class:: "<<Class[i]<<endl;
+cout<<"Course:: "<<course[i]<<endl;
+cout<<"Mobile No.:: "<<mobile_no[i]<<endl;
+cout<<"Admission Year:: "<<admission_year[i]<<endl;
+cout<<"\nEnter New Data::"<<endl<<endl;
+cout<<"Enter Roll No.:: ";
 cin>>roll_no[i];
-cout<<"Enter Name: ";
+cout<<"Enter Name:: ";
 cin>>name[i];
-cout<<"Enter Class: ";
+cout<<"Enter Class:: ";
 cin>>Class[i];
-cout<<"Enter Course: ";
+cout<<"Enter Course:: ";
 cin>>course[i];
-cout<<"Enter Mobile NO: ";
+cout<<"Enter Mobile No.:: ";
 cin>>mobile_no[i];
-cout<<"Enter Admission Year: ";
+cout<<"Enter The Year Of Admission:: ";
 cin>>admission_year[i];
 }
 }

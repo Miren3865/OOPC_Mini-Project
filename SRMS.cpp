6c39e8c68881d0f3f3689e06390a7f4f66bd2d56
Miren Savani <148608354+Miren3865@users.noreply.github.com>
2024-05-01 18:14:02 +0530

    Update SRMS.cpp

--- SRMS.cpp
+++ SRMS.cpp
@@ -13,18 +13,18 @@ if(total==0)
 total=ch+total;
 for(int i=0;i<ch;i++)
 {
-cout<<"\nEnter the Data of student "<<i+1<<endl<<endl;
-cout<<"Enter Roll NO:: "<<endl;
+cout<<"\nEnter The Data Of Student "<<i+1<<endl<<endl;
+cout<<"Enter The Roll No. Of Student:: "<<endl;
 cin>>roll_no[i];
-cout<<"Enter Name: "<<endl;
+cout<<"Enter The Name Of Student:: "<<endl;
 cin>>name[i];
-cout<<"Enter Class: "<<endl;
+cout<<"Enter Class:: "<<endl;
 cin>>Class[i];
-cout<<"Enter Course: "<<endl;
+cout<<"Enter Course:: "<<endl;
 cin>>course[i];
-cout<<"Enter Mobile NO: "<<endl;
+cout<<"Enter Mobile No.:: "<<endl;
 cin>>mobile_no[i];
-cout<<"Enter Admission Year: "<<endl;
+cout<<"Enter The Year Of Admission:: "<<endl;
 cin>>admission_year[i];
 }
 }
@@ -34,17 +34,17 @@ for(int i=total;i<ch+total;i++)
 //to store onward students data rather than storing double data()
 {
 cout<<"\nEnter the Data of student "<<i+1<<endl<<endl;
-cout<<"Enter Roll NO ";
+cout<<"Enter The Roll No. Of Student:: ";
 cin>>roll_no[i];
-cout<<"Enter Name: ";
+cout<<"Enter The Name Of Student:: ";
 cin>>name[i];
 cout<<"Enter Class: ";
 cin>>Class[i];
 cout<<"Enter Course: ";
 cin>>course[i];
-cout<<"Enter Mobile NO: ";
+cout<<"Enter Mobile No.: ";
 cin>>mobile_no[i];
-cout<<"Enter Admission Year: ";
+cout<<"Enter The Year Of Admission:: ";
 cin>>admission_year[i];
 }
 total=ch+total; //making choice
@@ -61,12 +61,12 @@ else
 for(int i=0;i<total;i++)
 {
 cout<<"\nData of Student "<<i+1<<endl<<endl;
-cout<<"Roll NO "<<roll_no[i]<<endl;
-cout<<"Name "<<name[i]<<endl;
-cout<<"Class "<<Class[i]<<endl;
-cout<<"Course "<<course[i]<<endl;
-cout<<"Mobile NO "<<mobile_no[i]<<endl;
-cout<<"Admission Year "<<admission_year[i]<<endl;
+cout<<"Roll No.: "<<roll_no[i]<<endl;
+cout<<"Name: "<<name[i]<<endl;
+cout<<"Class: "<<Class[i]<<endl;
+cout<<"Course: "<<course[i]<<endl;
+cout<<"Mobile No.: "<<mobile_no[i]<<endl;
+cout<<"Admission Year: "<<admission_year[i]<<endl;
 }
 }
 }
